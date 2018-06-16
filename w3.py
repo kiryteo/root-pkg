bc07d54c88b69285d68dc907dcc0a1465c3d6565
Ashwin Samudre <capnoi8@gmail.com>
2018-06-16 14:07:06 +0000

    db_source generating file

---
+++ w3.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+cwd = os.getcwd()
+
+rootdir = cwd
+
+rule1 = re.compile('.*name:.*')
+rule2 = re.compile('.*deps:.*')
+rule3 = re.compile('.*path:.*')
+
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+    	if file.endswith(".yaml"):
+    		fp = os.path.join(subdir, file)
+#    		print(fp)
+    		with open(fp) as t:
+    			fl = t.read()
+    			r1 = rule1.findall(fl)
+    			r2 = rule2.findall(fl)
+    			r3 = rule3.findall(fl)
+    			if r1:
+    				fl2 = open("new.yml", 'a')
+    				fl2.write(",".join(r1))
+    				fl2.write("\n")
+    			if r1:
+    				fl2 = open("new.yml", 'a')
+    				fl2.write(",".join(r2))
+    				fl2.write("\n")
+    			if r1:
+    				fl2 = open("new.yml", 'a')
+    				fl2.write(",".join(r3))
+    				fl2.write("\n")
