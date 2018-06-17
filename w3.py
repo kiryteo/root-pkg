8d5bef9598ca6dd301d0b7c2d7c5becce212b9ad
Ashwin Samudre <capnoi8@gmail.com>
2018-06-17 12:19:55 +0000

    proper yml format

--- w3.py
+++ w3.py
@@ -12,23 +12,25 @@ rule3 = re.compile('.*path:.*')
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-    	if file.endswith(".yaml"):
-    		fp = os.path.join(subdir, file)
-#    		print(fp)
-    		with open(fp) as t:
-    			fl = t.read()
-    			r1 = rule1.findall(fl)
-    			r2 = rule2.findall(fl)
-    			r3 = rule3.findall(fl)
-    			if r1:
-    				fl2 = open("new.yml", 'a')
-    				fl2.write(",".join(r1))
-    				fl2.write("\n")
-    			if r1:
-    				fl2 = open("new.yml", 'a')
-    				fl2.write(",".join(r2))
-    				fl2.write("\n")
-    			if r1:
-    				fl2 = open("new.yml", 'a')
-    				fl2.write(",".join(r3))
-    				fl2.write("\n")
+        if file.endswith(".yaml"):
+            fp = os.path.join(subdir, file)
+#           print(fp)
+            with open(fp) as t:
+                fl = t.read()
+                r1 = rule1.findall(fl)
+                r1 = [x.strip(' name: ') for x in r1 ]
+                r2 = rule2.findall(fl)
+                r3 = rule3.findall(fl)
+                if r1:
+                    fl2 = open("new.yml", 'a')
+                    fl2.write(",".join(r1))
+                    fl2.write(":")
+                    fl2.write("\n")
+                if r1:
+                    fl2 = open("new.yml", 'a')
+                    fl2.write(",".join(r2))
+                    fl2.write("\n")
+                if r1:
+                    fl2 = open("new.yml", 'a')
+                    fl2.write(",".join(r3))
+                    fl2.write("\n")
